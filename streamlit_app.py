dc81b16b127ba3dd8a872092a6e833d1bd0dbb13
Behzod Jumaev <119079707+BJ31122002@users.noreply.github.com>
2024-10-05 18:46:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('Test app')
 
 st.write('Hello world!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')\
+print(df)
