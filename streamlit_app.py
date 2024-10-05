2c9fa1e48fe3c7b07311840a3bf2b89784cb4a20
Behzod Jumaev <119079707+BJ31122002@users.noreply.github.com>
2024-10-05 19:25:42 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,29 @@ with st.expander('Initial data'):
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
   st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
+
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies (input_penguins, prefix=encode)
+
+X = df_penguins [1:]
+input_row = df_penguins [:1]
+# Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
+def target_encode(val):
+  return target_mapper [val]
+y = y_raw.apply(target_encode)
+with st.expander ('Data preparation'):
+  st.write('**Encoded X (input penguin)**')
+  input_row
+  st.write('**Encoded y**')
+  y
+
+  st.header('Inout Features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgerson'))
+  bill_length_mm = st.slider('Bill depth (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2) 
+  flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
